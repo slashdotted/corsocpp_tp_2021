7aa15ba8de2af59a3f2a49bf3711702f872f250c
Amos Brocco <me@amosbrocco.ch>
2021-03-08 14:01:55 +0100

    Fine lezione 8.3.2021

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
-#include "arrays.h"
-#include "functions.h"
-#include "heap.h"
-#include "namespace.h"
-#include "references.h"
-#include "strings.h"
-#include "variables.h"
+#include "esempio_struct.h"
+#include <iostream>
+
+void stampa(const Frazione &f) { f.scrivi(); }
+
+void debug(const Frazione &f) {
+  std::cout << "debug: num=" << f.num << " den=" << f.den << '\n';
+}
 
 int main() {
-  namespace_run();
-  // functions_run();
-  strings_run();
-  variables_run();
-  arrays_run();
-  heap_run();
-  references_run();
+  Frazione f;
+  {                   // default 0/1
+    Frazione g{5, 7}; // 5/7
+  }
+
+  std::cout << Frazione::numero_istanze() << '\n';
+  Frazione h{8}; // 8/1
+  debug(h);
+  auto *j{new Frazione};
+  std::cout << Frazione::numero_istanze() << '\n';
+  // f.numeratore(5);
+  // f.denominatore(7);
+  // f.den = 0;
+  delete j;
+  // f.num = 0;
+  stampa(f);
+  std::cout << Frazione::numero_istanze() << '\n';
+  f.numeratore();
+  Frazione::numero_istanze();
   return 0;
 }
---
+++ src/structenum.h
@@ -0,0 +1,1 @@
+void structenum_run();
