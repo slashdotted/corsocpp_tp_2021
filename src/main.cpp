22fa6164a6301f4f28d0459c3ec9fe0b7e9186fa
Amos Brocco <me@amosbrocco.ch>
2021-03-22 14:35:56 +0100

    shared_ptr

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
-#include "smartpointers.h"
+#include "smartpointers_shared.h"
+#include "smartpointers_unique.h"
 
-int main() { smartpointers_run(); }
+int main() {
+  smartpointers_unique_run();
+  smartpointers_shared_run();
+}
