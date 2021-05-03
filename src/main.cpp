94a0396743691741df97bc2089fce7bf51beb977
Amos Brocco <me@amosbrocco.ch>
2021-05-03 13:21:45 +0200

    Template: introduzione

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 #include "mylist.h"
+#include <typeinfo>
 
 using std::cout;
 
-int main() {
-  MyList ml{10};
-  for (const auto& i : {0,1,2,3,4,5,6,7,8,9}) {
-    ml[i] = i;
-  }
-  for (const auto& i : {0,1,2,3,4,5,6,7,8,9}) {
-    cout << ml[i] << '\n';
-  }
+template<typename X, typename Y>
+struct MioTipo {
+  X x;
+  Y y;
+};
 
+template<typename X = int, typename Y = bool>
+struct MioTipoConTipiDefault {
+  X x;
+  Y y;
+
+  void tipi() const {
+    cout << "X=" << typeid(x).name() << " Y=" << typeid(y).name() << '\n';
+  }
+};
+
+int main() {
+  MioTipoConTipiDefault<int, double> k;
+  k.tipi();
+  MioTipoConTipiDefault<int> p;
+  p.tipi();
+  MioTipoConTipiDefault<> a;
+  a.tipi();
+
+  MyList<int, 10> ml;
+  MyList<int, 15> ql;
+  cout << typeid(ml).name() << " - " << typeid(ql).name() << '\n';
+  MyList<double, 6> dl;
 }
--- src/mylist.h
+++ src/mylist.h
@@ -1,18 +1,57 @@
 #include <stdexcept>
 #include <iostream>
+
+
+template<typename T, int Size = 200>
 class MyList {
 public:
-    explicit MyList(int size);
+    explicit MyList();
     MyList(const MyList &o)=delete;
     MyList(const MyList &&o)=delete;
-    int& operator=(const MyList &o)=delete;
-    int& operator=(const MyList &&o)=delete;
+    T& operator=(const MyList &o)=delete;
+    T& operator=(const MyList &&o)=delete;
     virtual ~MyList() noexcept;
-    int& operator[](int i);
-    const int& operator[](int i)const;
+    T& operator[](int i);
+    const T& operator[](int i)const;
     int size()const;
 
 private:
-    int *m_array;
-    int m_size;
+    T *m_array;
 };
+
+template<typename T, int Size>
+MyList<T,Size>::MyList() {
+    if(Size<=0) {
+        throw std::out_of_range("Invalid size");
+    }
+    m_array=new T[Size];
+}
+
+template<typename T, int Size>
+const T& MyList<T,Size>::operator[](int i) const
+{
+    if(i<0||i>=Size)
+    {
+        throw std::out_of_range("Invalid index");
+    }
+    return m_array[i];
+}
+
+template<typename T, int Size>
+T& MyList<T,Size>::operator[](int i) {
+    if(i<0||i>=Size) {
+        throw std::out_of_range("Invalid index");
+    }
+    return m_array[i];
+}
+
+template<typename T, int Size>
+int MyList<T,Size>::size()const {
+    return Size;
+}
+
+template<typename T, int Size>
+MyList<T,Size>::~MyList() noexcept {
+    delete[]m_array;
+}
+
